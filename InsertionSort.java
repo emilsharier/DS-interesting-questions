beb7ab7d7a98045fa17a3899ff3d605ed346e796
emilsharier <emilsharier@gmail.com>
2020-05-21 02:12:42 +0530

    Adding insertion sort

---
+++ InsertionSort.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ds.interesting.questions;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Emil
+ */
+public class InsertionSort {
+    
+    static int[] insertionSort(int a[]) {
+        int i, j, key;
+        for(i = 1; i < a.length; i ++) {
+            key = a[i];
+            j = i - 1;
+            while(j >= 0 && a[j] > key) {
+                a[j + 1] = a[j];
+                j --;
+            }
+            j ++;
+            if(j >= 0) {
+                a[j] = key;
+            }
+        }
+        return a;
+    }
+    
+    public static void main(String[] args) {
+        int a[] = new int[10];
+        int i;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the elements");
+        for(i = 0; i < 10; i ++)
+            a[i] = sc.nextInt();
+        a = insertionSort(a);
+        System.out.println("The sorted array elements are");
+        for(i = 0; i < 10; i ++)
+            System.out.print(a[i] + " ");
+        sc.close();
+    }
+}
